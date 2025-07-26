ce60d33b1d8695cf38d6dc6e0e1df0bcebab53a2
jwyxym <jwyxym@126.com>
2025-07-26 15:19:17 +0800

    Update premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -489,7 +489,7 @@ workspace "YGOPro"
         targetdir "bin/debug/x64"
 
     filter { "configurations:Release", "action:vs*" }
-        linktimeoptimization "On"
+        flags { "LinkTimeOptimization" }
         staticruntime "On"
         disablewarnings { "4244", "4267", "4838", "4996", "6011", "6031", "6054", "6262" }
 
